85034d900112ae01f6091b7c785fd262dcb3784a
quzhizhihou <3488053626@qq.com>
2022-03-20 16:39:49 +0800

    Update HelloWorld.java

--- EBU6304_G102/smart kiosks/src/HelloWorld.java
+++ EBU6304_G102/smart kiosks/src/HelloWorld.java
@@ -1,6 +1,10 @@
+import java.util.*;
+
 public class HelloWorld {
     public static void main(String args[])
     {
         System.out.println("Hello World");
+        
+        System.out.println("We are handsome!");
     }
 }
